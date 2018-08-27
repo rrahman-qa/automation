f287605715410cb511f38591f64f71daf9630cd9
Rahi <rrahman.qa@gmail.com>
2018-08-27 19:18:33 +0100

    removed redundant imports and class constructor

--- src/test/java/com/automation/NavigateTest.java
+++ src/test/java/com/automation/NavigateTest.java
@@ -1,20 +1,11 @@
 package com.automation;
 
-
 import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 public class NavigateTest extends BaseTest{
-
-    public NavigateTest(){
-        super();
-    }
-
+    
     @Test
     public void test(){
         driver.navigate().to("http://bbc.co.uk");
